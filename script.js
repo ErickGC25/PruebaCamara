efb28d131bba6f3b72ab0510f142a1cb96d8b690
Erick <eegarciac@televisa.com.mx>
2021-04-27 19:11:31 -0500

    Poniendo audio

--- script.js
+++ script.js
@@ -2,17 +2,16 @@
 
 
 document.addEventListener('DOMContentLoaded', ()=> {
-    const video = document.querySelector("#video");
+    // const video = document.querySelector("#video");
     const btnCamara = document.querySelector("#CameraAccess");
     btnCamara.addEventListener('click', ()=> {
-        console.log();
         navigator.mediaDevices.getUserMedia({
             audio: true,
             // video: {
             //     facingMode: { exact: "User" } //User / left / right
             // }
         }).then(stream => {
-            video.srcObject = stream;
+            // video.srcObject = stream;
         }).catch(console.error())
 
     })
