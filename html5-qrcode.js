b55ce225bd28738bfd10e579705b1f0f74f7e43c
Erick <eegarciac@televisa.com.mx>
2021-04-28 09:09:41 -0500

    Prueba 9

--- html5-qrcode.js
+++ html5-qrcode.js
@@ -1,38 +1,3 @@
-
-
-
-// document.addEventListener('DOMContentLoaded', ()=> {
-//     // const video = document.querySelector("#video");
-//     const btnCamara = document.querySelector("#CameraAccess");
-//     // btnCamara.addEventListener('click', ()=> {
-//         document.querySelector('#CameraAccess').addEventListener('click', async function init(e) {
-//             try {
-//               const stream = await navigator.mediaDevices.getUserMedia({
-//                 audio: false,
-//                 video: {
-
-//                     facingMode: { exact: "environment" } 
-//                 }
-//               })
-//               alert('Viendo video')
-//               const videoTracks = stream.getVideoTracks()
-//               const track = videoTracks[0]
-//               alert(`Getting video from: ${track.label}`)
-//               document.querySelector('video').srcObject = stream
-//               document.querySelector('#get-access').setAttribute('hidden', true)
-//               setTimeout(() => { track.stop() }, 3 * 1000)
-//             } catch (error) {
-//               alert(`${error.name}`)
-//               console.error(error)
-//               alert('Viendo video')
-//             }
-//           })
-          
-
-//     // })
-
-// })
-
 (function($) {
     jQuery.fn.extend({
         html5_qrcode: function(qrcodeSuccess, qrcodeError, videoError) {
@@ -91,7 +56,6 @@
 
                 qrcode.callback = function (result) {
                     qrcodeSuccess(result, localMediaStream);
-                    alert(result);
                 };
             }); // end of html5_qrcode
         },
@@ -107,4 +71,3 @@
         }
     });
 })(jQuery);
-
